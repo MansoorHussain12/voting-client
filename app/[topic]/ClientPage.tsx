7d6095e76faa92e76b6c8bdead27121760e281e1
Mansoor Hussain <qaisrani07@gmail.com>
2024-06-10 19:06:16 +0500

    implement socket connection

--- app/[topic]/ClientPage.tsx
+++ app/[topic]/ClientPage.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Wordcloud } from "@visx/wordcloud";
 import { scaleLog } from "@visx/scale";
 import { Text } from "@visx/text";
@@ -10,6 +10,9 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useMutation } from "@tanstack/react-query";
 import { submitComment } from "../actions";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:8080");
 
 interface Props {
   topicName: string;
@@ -21,6 +24,44 @@ const colors = ["#143059", "#2F6B9A", "#82A6C2"];
 const ClientPage = ({ topicName, initialData }: Props) => {
   const [words, setWords] = useState(initialData);
   const [input, setInput] = useState("");
+
+  useEffect(() => {
+    socket.emit("join-room", `room:${topicName}`);
+  }, []);
+
+  useEffect(() => {
+    socket.on("room-update", (message: string) => {
+      const data = JSON.parse(message) as {
+        text: string;
+        value: number;
+      }[];
+
+      data.map((newWord) => {
+        const isWordAlreadyIncluded = words.some(
+          (word) => word.text === newWord.text
+        );
+
+        if (isWordAlreadyIncluded)
+          setWords((prev) => {
+            const before = prev.find((word) => word.text === newWord.text);
+            const rest = prev.filter((word) => word.text !== newWord.text);
+
+            return [
+              ...rest,
+              { text: before!.text, value: before!.value + newWord.value },
+            ];
+          });
+        else if (words.length < 50) {
+          // add to state
+          setWords((prev) => [...prev, newWord]);
+        }
+      });
+    });
+
+    return () => {
+      socket.off("room-update");
+    };
+  }, [words]);
 
   const fontScale = scaleLog({
     domain: [
@@ -39,41 +80,43 @@ const ClientPage = ({ topicName, initialData }: Props) => {
       <MaxWidthWrapper className="flex flex-col items-center gap-6 pt-20">
         <h1 className="text-4xl sm:text-5xl font-bold text-center tracking-tight text-balance">
           What people think about{" "}
-          <span className="text-blue-600">{topicName}</span>
+          <span className="text-blue-600">{topicName}</span>:
         </h1>
+
         <p className="text-sm">(updated in real-time)</p>
 
         <div className="aspect-square max-w-xl flex items-center justify-center">
           <Wordcloud
             words={words}
-            width={500}
-            height={500}
+            width={300}
+            height={300}
             fontSize={(data) => fontScale(data.value)}
-            font="Impact"
+            font={"Impact"}
             padding={2}
             spiral="archimedean"
             rotate={0}
             random={() => 0.5}
           >
             {(cloudWords) =>
-              cloudWords.map((word, index) => (
+              cloudWords.map((w, i) => (
                 <Text
-                  key={index}
-                  fill={colors[index % colors.length]}
+                  key={w.text}
+                  fill={colors[i % colors.length]}
                   textAnchor="middle"
-                  transform={`translate(${word.x}, ${word.x})`}
-                  fontSize={word.size}
-                  fontFamily={word.font}
+                  transform={`translate(${w.x}, ${w.y})`}
+                  fontSize={w.size}
+                  fontFamily={w.font}
                 >
-                  {word.text}
+                  {w.text}
                 </Text>
               ))
             }
           </Wordcloud>
         </div>
+
         <div className="max-w-lg w-full">
           <Label className="font-semibold tracking-tight text-lg pb-2">
-            Here is what I think about {topicName}
+            Here's what I think about {topicName}
           </Label>
           <div className="mt-1 flex gap-2 items-center">
             <Input
